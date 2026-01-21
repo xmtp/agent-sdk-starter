425bd0903161d0fb0446957bcc82968e16515486
Benny Neugebauer <mail@bennycode.com>
2026-01-22 00:43:43 +0100

    rc2

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const router = new CommandRouter();
 
 router.command('/version', async ctx => {
   const libXmtpVersion = ctx.client.libxmtpVersion;
-  await ctx.sendText(`My libXMTP version is: ${libXmtpVersion}`);
+  await ctx.conversation.sendText(`My libXMTP version is: ${libXmtpVersion}`);
 });
 
 router.command('/send-image', async ctx => {
@@ -30,10 +30,10 @@ router.command('/send-image', async ctx => {
     });
 
     const mimeType = 'application/octet-stream';
-    const encryptedBlob = new Blob([Buffer.from(attachment.content.payload)], {
+    const encryptedBlob = new Blob([Buffer.from(attachment.payload)], {
       type: mimeType,
     });
-    const encryptedFile = new File([encryptedBlob], attachment.filename, {
+    const encryptedFile = new File([encryptedBlob], attachment.filename || 'untitled', {
       type: mimeType,
     });
     const upload = await pinata.upload.public.file(encryptedFile);
@@ -45,7 +45,7 @@ router.command('/send-image', async ctx => {
 });
 
 agent.on('attachment', async ctx => {
-  const receivedAttachment = await downloadRemoteAttachment(ctx.message.content, agent);
+  const receivedAttachment = await downloadRemoteAttachment(ctx.message.content);
   console.log(`Received attachment: ${receivedAttachment.filename}`);
 });
 
@@ -58,15 +58,15 @@ agent.on('reply', ctx => {
 });
 
 agent.on('text', async ctx => {
-  await ctx.sendText(`Echo: ${ctx.message.content}`);
+  await ctx.conversation.sendText(`Echo: ${ctx.message.content}`);
 });
 
 agent.on('dm', async ctx => {
-  await ctx.sendText('Hello you!');
+  await ctx.conversation.sendMarkdown('**Hello you!**');
 });
 
 agent.on('group', async ctx => {
-  await ctx.sendText('Hello group!');
+  await ctx.conversation.sendText('**Hello group!**');
 });
 
 agent.on('unhandledError', (error: unknown) => {
